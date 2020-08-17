f5fb3ac035f81c5c04db3e58381ad8b315bba1fd
Eyal Barkai <eyalskl18@gmail.com>
2020-08-17 17:13:57 +0300

    Updates

---
+++ precache-manifest.08a22d5347baf4365753c6bc6c2d9423.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "390795a5786e305384644073f4cc7798",
+    "url": "/misterBitcoin-react/index.html"
+  },
+  {
+    "revision": "ad3b6a75543c4711d6d9",
+    "url": "/misterBitcoin-react/static/css/main.664a5d93.chunk.css"
+  },
+  {
+    "revision": "afda9fe4d162275a016b",
+    "url": "/misterBitcoin-react/static/js/2.30132c69.chunk.js"
+  },
+  {
+    "revision": "2f90de1fa437077acc84e59746ddc970",
+    "url": "/misterBitcoin-react/static/js/2.30132c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad3b6a75543c4711d6d9",
+    "url": "/misterBitcoin-react/static/js/main.097e611f.chunk.js"
+  },
+  {
+    "revision": "d3756071b02b654c3d7c",
+    "url": "/misterBitcoin-react/static/js/runtime-main.3bc4e8e9.js"
+  }
+]);
